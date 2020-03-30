08c1e74b76fd9d2645f0a877f01f7484d7e6c91d
ichizok <gclient.gaap@gmail.com>
2020-03-30 18:52:36 +0900

    Modify main.c

--- main.c
+++ main.c
@@ -8,9 +8,14 @@
 void use_gethostbyname(const char *name)
 {
     struct hostent *host = gethostbyname(name);
-    if (host != NULL) {
+    char **p;
+    if (host == NULL) {
+        fprintf(stderr, "gethostbyname: %s\n", strerror(h_errno));
+        return;
+    }
+    for (p = host->h_addr_list; *p != NULL; ++p) {
         struct in_addr inaddr = {0};
-        memcpy(&inaddr, host->h_addr_list[0], host->h_length);
+        memcpy(&inaddr, *p, host->h_length);
         printf("gethostbyname:\n %s is %s\n", name, inet_ntoa(inaddr));
     }
 }
@@ -19,13 +24,17 @@ void use_getaddrinfo(const char *name)
     int err;
     struct addrinfo hints = {0};
     struct addrinfo *res = NULL;
+    struct addrinfo *r = NULL;
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     if ((err = getaddrinfo(name, NULL, &hints, &res)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(err));
         return;
     }
-    printf("getaddrinfo:\n %s is %s\n", name, inet_ntoa(((struct sockaddr_in *)res->ai_addr)->sin_addr));
+    for (r = res; r != NULL; r = r->ai_next) {
+        printf("getaddrinfo:\n %s is %s\n", name, inet_ntoa(((struct sockaddr_in *)r->ai_addr)->sin_addr));
+    }
+    freeaddrinfo(res);
 }
 int main(int argc, char *argv[])
 {
